9703748a6ae90acb99fa4c9cdeee515b790f0e32
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-07-20 01:19:58 -0300

    kj

---
+++ Aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+if (vel > 60) {
+    console.log('Você foi Multado!')
+}
+
+console.log('Dirija sempre usando o cinto de segurança!')
---
+++ Aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo no país ${pais}.`)
+if (pais != 'Brasil') {
+    console.log('Estrangeiro!')
+} else {
+    console.log('Brasileiro!')
+}
