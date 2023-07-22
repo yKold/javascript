74c14709bbe7a1b083ebb768b5d7fd9bf7981361
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-07-22 00:13:35 -0300

    y

--- Aula014/script.js
+++ Aula014/script.js
@@ -9,6 +9,50 @@ function resultado() {
     } else {
         var fsex = document.getElementsByName("sex")
         var idade = ano - Number(dano.value)
-        res.innerHTML = `Idade Calculada: ${idade}`
+        // res.innerHTML = `Idade Calculada: ${idade}`
+
+        var genero = ' '
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade < 5){
+                img.setAttribute('src', 'imagens/H bebe.jpg') 
+                img.style.width = '200px'
+            } else if (idade < 13) {
+                img.setAttribute('src', 'imagens/H criança.jpg')
+                img.style.width = '200px'
+            } else if (idade < 20) {
+                img.setAttribute('src', 'imagens/H jovem.jpg')
+                img.style.width = '200px'
+            } else if (idade < 60) {
+                img.setAttribute('src', 'imagens/H adulto.jpg')
+                img.style.width = '200px'
+            } else if (idade >= 60) {
+                img.setAttribute('src', 'imagens/H idoso.jpg')
+                img.style.width = '200px'
+            } 
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade < 5){
+                img.setAttribute('src', 'imagens/M bebe.jpg') 
+                img.style.width = '200px'
+            } else if (idade < 13) {
+                img.setAttribute('src', 'imagens/M criança.jpg')
+                img.style.width = '200px'
+            } else if (idade < 20) {
+                img.setAttribute('src', 'imagens/M jovem.jpg')
+                img.style.width = '200px'
+            } else if (idade < 60) {
+                img.setAttribute('src', 'imagens/M adulto.jpg')
+                img.style.width = '200px'
+            } else if (idade >= 60) {
+                img.setAttribute('src', 'imagens/M idoso.jpg')
+                img.style.width = '200px'
+            } 
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
