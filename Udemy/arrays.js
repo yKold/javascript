501c27aaa58b1b1e462e7124d9ae61cf49e3ca0b
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-08-30 21:51:34 -0300

    nijk

---
+++ Udemy/arrays.js
@@ -0,0 +1,5 @@
+var list = ['batata', 'cenoura']
+
+list.concat(['cebola']) //este metodo concatena um array ao outro, adiciona a cebola na lista.
+
+list.sort() // reorganiza a lista, mas apaenas a original
