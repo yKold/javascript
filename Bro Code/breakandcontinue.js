1ec1052b2a33d74f123d2a38b9fc8bd737611183
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-09-01 11:41:43 -0300

    j

---
+++ Bro Code/breakandcontinue.js
@@ -0,0 +1,9 @@
+for(let i = 1; i <= 20; i++){
+    if(i == 13){
+        continue
+    } 
+    console.log(i)
+}
+
+// BREAK = breakes in the momento
+// CONTINUE = break in the value and continue after
