7a1db97a36b6461ed7079d0a906209854530c499
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-09-05 23:25:01 -0300

    Create conditions2.js

---
+++ Udemy/conditions2.js
@@ -0,0 +1,17 @@
+// condition ? "if its true" : "if its false"
+
+function user(bool){
+    return bool;
+}
+var answer = user(true) ? 'bom' : 'ruim'
+
+switch(direcao) {
+    case 'frente':
+        console.log("bla bla bla")
+        break
+    case 'tras':
+        console.log("bla bla bla")
+        break
+    default:
+        console.log("bla bla bla")
+}
