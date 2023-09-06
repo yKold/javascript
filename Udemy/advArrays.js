71475a6e49e48b1f24d753b5c26079e06fe8bb12
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-09-06 20:53:53 -0300

    Create advArrays.js

---
+++ Udemy/advArrays.js
@@ -0,0 +1,20 @@
+// metodo chato
+const array = [1, 2, 10, 16]
+const double = []
+const newArray = array.forEach((num) => double.push(num * 2))
+
+// metodo fácil
+const newArray2 = array.map((num) => num*2)
+addEventListener('click', (a) => {
+    console.log('amor')
+})      
+
+// filter
+const filterArray = array.filter(num => {
+    return num > 5
+})
+
+// reduce
+const reduceArray = array.reduce((accumulator, num) => {
+    return accumulator + num
+}, 0) // o valor '0' é pq o acumulador começa em 0 e soma com os arrays
