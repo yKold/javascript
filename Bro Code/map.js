ce01c7018481cd2eb0d2d5e809a88893c2b9f1bf
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2023-09-10 20:53:12 -0300

    Create map.js

---
+++ Bro Code/map.js
@@ -0,0 +1,12 @@
+// Map = objeto que segura dois elementos, um par de elementos
+
+const store = new Map([
+    ['tshirt', 20]
+    ['jeans', 30]
+])
+
+// store.get('tshirt') = devolve '20'
+// store.set('hat', 40)
+// store.delete('hat')
+// store.has('underwear')
+// sotre.size
